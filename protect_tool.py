93ee7e0735f9bb20ba159f1e6ee8db565560fd30
neeltanu2010 <neeltanu2010@gmail.com>
2026-06-17 23:10:29 +0530

    Update protect_tool.py

--- protect_tool.py
+++ protect_tool.py
@@ -53,7 +53,7 @@ def get_current_access(tool_name: str) -> dict:
 
     return _post("/usage/check", {
         "email": email,
-        "tool": tool_name
+        "tool_name": tool_name
     })
 
 
@@ -64,7 +64,7 @@ def record_tool_use(tool_name: str):
 
     return _post("/usage/record", {
         "email": email,
-        "tool": tool_name
+        "tool_name": tool_name
     })
 
 
